72a7ba9435b840af3e249455d66c01c8c068b0d9
mininene <iranzof@gmail.com>
2020-02-27 19:12:33 +0100

    finished

---
+++ ExamenFinalVuelingIF/Services/Log/DevelopmentLog.cs
@@ -0,0 +1,26 @@
+﻿using ExamenFinalVuelingIF.InfraestructuraTransversal.ServicesException;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenFinalVuelingIF.Services.Log
+{
+    public class DevelopmentLog:ILog
+    {
+        public void WriteLog(string mensaje)
+        {
+
+            var date = DateTime.Now;
+
+            try
+            {
+                Console.WriteLine($"[{date}] {mensaje}");
+            }
+            catch (Exception ex)
+            {
+                throw new LogException("No se ha podido escribir en la consola", ex);
+            }
+        }
+    }
+}
